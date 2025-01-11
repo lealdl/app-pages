49f3149484f12db3666149459b61f166b693c641
Luciano <lucianodleal@gmail.com>
2025-01-11 00:47:30 -0300

    atualização tamanho fonte do rodapé

--- src/componentes/footer.js
+++ src/componentes/footer.js
@@ -2,10 +2,10 @@
 function Footer() {
   return `
     <footer class="bg-[#012025] py-8 text-center text-white">
-      <p class="text-lg">© 2025 Clinihard Sistemas. (41)98903-1310 Todos os direitos reservados.</p>
+      <p class="text-xs">© 2025 Clinihard Sistemas. (41)98903-1310 Todos os direitos reservados.</p>
       <div class="mt-4">
         <!-- Conteúdo adicional pode ser colocado aqui -->
-        sistemas.clinihard@gmail.com
+        <p class="text-xs"> sistemas.clinihard@gmail.com
       </div>
     </footer>
   `;
