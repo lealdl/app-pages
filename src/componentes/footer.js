640209894cac8f578a966a6ddb18cdfd2be90163
lealdl <69693285+lealdl@users.noreply.github.com>
2025-02-13 01:15:27 -0300

    correção ano automático no footer

--- src/componentes/footer.js
+++ src/componentes/footer.js
@@ -1,11 +1,11 @@
-// src/components/footer.js
 function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return `
     <footer class="bg-[#012025] py-8 text-center text-white">
-      <p class="text-xs">© 2025 Clinihard Sistemas. (41)98903-1310 Todos os direitos reservados.</p>
+      <p class="text-xs">© ${currentYear} Clinihard Sistemas. (41) 98903-1310</p>
       <div class="mt-4">
-        <!-- Conteúdo adicional pode ser colocado aqui -->
-        <p class="text-xs"> sistemas.clinihard@gmail.com
+        <p class="text-xs">sistemas.clinihard@gmail.com</p>
       </div>
     </footer>
   `;
