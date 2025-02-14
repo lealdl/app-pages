367413d8adca9361540842e59f4441833f3b17cf
lealdl <69693285+lealdl@users.noreply.github.com>
2025-02-14 02:52:48 -0300

    ajustes footer ano automático

--- src/componentes/footer.js
+++ src/componentes/footer.js
@@ -1,4 +1,5 @@
 function Footer() {
+  const currentYear = new Date().getFullYear();
   const whatsappNumber = "5541989031310";
   const whatsappMessage = encodeURIComponent(
     "Olá Clinihard Sistemas! Gostaria de mais informações sobre os aplicativos que desenvolvem"
@@ -9,7 +10,7 @@ function Footer() {
     <footer class="bg-[#012025] py-4 text-center text-white text-xs">
       <!-- Mobile: Cada item em uma linha separada -->
       <div class="md:hidden block">
-        <p>© 2025 Clinihard Sistemas</p>
+        <p>© ${currentYear} Clinihard Sistemas</p>
         <p>
           <a href="${whatsappLink}" target="_blank" class="flex items-center justify-center gap-2">
             <img src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg" alt="WhatsApp" class="w-4 h-4">
@@ -21,7 +22,7 @@ function Footer() {
 
       <!-- Desktop: Tudo em uma linha com separadores -->
       <p class="hidden md:flex justify-center items-center gap-2">
-        © 2025 Clinihard Sistemas ||
+        © ${currentYear} Clinihard Sistemas ||
         <a href="${whatsappLink}" target="_blank" class="flex items-center gap-2">
           <img src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg" alt="WhatsApp" class="w-4 h-4">
           (41) 98903-1310
