7fd220f2e39332005cf5682cc54d8446db43627d
lealdl <69693285+lealdl@users.noreply.github.com>
2025-06-27 03:49:59 -0300

    ajustes footer

--- src/componentes/footer.js
+++ src/componentes/footer.js
@@ -1,14 +1,12 @@
-// src/components/footer.js
 function Footer() {
   return `
-    <footer class="bg-[#012025] py-8 text-center text-white">
-      <p class="text-lg">© 2025 Clinihard Sistemas. (41)98903-1310 Todos os direitos reservados.</p>
-      <div class="mt-4">
+    <footer class="bg-[#012025] py-4 text-center text-white">
+      <p class="text-xs">©2025 Clinihard Sistemas. (41)98903-1310 Todos os direitos reservados.</p>
+      <div class="mt-4 text-xs">
         <!-- Conteúdo adicional pode ser colocado aqui -->
         sistemas.clinihard@gmail.com
       </div>
     </footer>
   `;
 }
-
 export default Footer;
